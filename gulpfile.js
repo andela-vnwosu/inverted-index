00e176c68c780edf3ea7c10fe26307018c307019
Chiamaka Nwosu <chiamaka.nwosu@andela.com>
2017-02-02 17:08:28 +0100

    adds new instatnce of gulp.task browserTest

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const browserSync = require ('browser-sync').create();
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const browserTest = require ("browser-sync").create();
 
 gulp.task('browserSync', ['watch'], function() {
     browserSync.init({
@@ -7,18 +10,37 @@ gulp.task('browserSync', ['watch'], function() {
             baseDir: './public',
         },
         port: process.env.PORT || 3000,
+      ui: false,
         ghostMode: false
     });
 });
 
+gulp.task('browserTest', () => {
+  browserTest.init({
+    server: {
+      baseDir: ['./public/src', './jasmine'],
+      index: 'SpecRunner.html'
+    },
+    port: 8888,
+    ui: false,
+    ghostMode: false
+  });
+});
 
 
-gulp.task('default', ['browserSync', 'watch']);
+gulp.task('default', ['browserSync', 'browserTest', 'watch']);
 
 gulp.task('watch', function() {
 
   gulp.watch('public/css/index.css', browserSync.reload);
   gulp.watch('public/index.html', browserSync.reload);
-  gulp.watch('jasmine/spec/inverted-index-test.js', browserSync.reload);
+  gulp.watch('jasmine/spec/tests/test-spec.js', browserSync.reload);
   gulp.watch('./src/*.js', browserSync.reload);
 });
+
+gulp.task('browserify', () =>
+  browserify('jasmine/spec/inverted-index-test.js')
+    .bundle()
+    .pipe(source('test-spec.js'))
+    .pipe(gulp.dest('./jasmine/spec/tests'))
+);
