d2486878c49f0325d3d7e5ef48f188133e605f0c
Chiamaka Nwosu <chiamaka.nwosu@andela.com>
2017-02-06 00:48:53 +0100

    refactor code

--- public/src/inverted-index.js
+++ public/src/inverted-index.js
@@ -18,48 +18,49 @@ class InvertedIndex {
    * Create `index
    *
    * creates an inverted index from the specified fileName or JsonObject
-   *
-   * @param {String} fileName description
-   * @param {Object} jsonObject descr
+   * @param {Object} jsonObject the json data to build
    * @return {Object} index that was built
    */
-  createIndex(fileName, jsonObject) {
+  createIndex(jsonObject) {
     const index = {};
     jsonObject.map((sentence, count) => {
-      (`${sentence.title} ${sentence.text}`)
-        .replace(/[^A-Za-z0-9\s]/g, '')
-        .toLowerCase().split(' ')
-        .map((word) => {
-          if (index[word] && index[word].indexOf(count) === -1) {
-            index[word].push(count);
-          } else {
-            index[word] = [count];
-          }
-        });
+      this.filterString((`${sentence.title} ${sentence.text}`))
+      .split(' ')
+      .map((word) => {
+        if (index[word] && !index[word][count]) {
+          index[word].push(count);
+        } else {
+          index[word] = [count];
+        }
+      });
     });
     this.indices.push(index);
     return index;
   }
 
+  filterString(str) {
+    return str.replace(/[^A-Za-z0-9\s]/g, '')
+        .toLowerCase();
+  }
+
   /**
    * Searches the recently indexed object for matches with specified parameters
    * @method searchIndex
-   * @param {String} term
-   * @param {String} choice
-   * @return {Array} result
+   * @param {String} term terms to search for
+   * @param {String} selectedFile the specific file to search in
+   * @return {Array} result 
    */
-  searchIndex(term, choice) {
+  searchIndex(term, selectedFile) {
     this.temp_search = [];
     const result = [];
     this.resolveSearchTerms(term);
     term = this.search_terms.toString().replace(',', ' ');
     this.search_terms = [];
-    const terms = term.replace(/[^A-Za-z0-9\s]/g, '').toLocaleLowerCase().split(' ');
-    console.log(term);
-    if (typeof choice === 'undefined') {
-      choice = 'all';
+    const terms = this.filterString(term).split(' ');
+    if (typeof selectedFile === 'undefined') {
+      selectedFile = 'all';
     }
-    if (choice === 'all') {
+    if (selectedFile === 'all') {
       this.indices.map((index, pos) => {
         result[pos] = {};
         let savedWord;
@@ -74,15 +75,15 @@ class InvertedIndex {
         });
       });
     } else {
-      choice = parseInt(choice);
-      const currentIndex = this.indices[choice];
+      selectedFile = parseInt(selectedFile);
+      const currentIndex = this.indices[selectedFile];
       result[0] = {};
       let savedWord;
       terms.map((word) => {
         for (savedWord in currentIndex) {
           if (savedWord === word) {
-            result[0][word] = this.indices[choice][savedWord];
-            this.temp_search.push(this.indices[choice][savedWord][0]);
+            result[0][word] = this.indices[selectedFile][savedWord];
+            this.temp_search.push(this.indices[selectedFile][savedWord][0]);
           }
         }
       });
@@ -116,6 +117,31 @@ class InvertedIndex {
       return false;
     }
   }
+
+  /**
+   * Asserts if an object is not empty and if it contains the property text
+   * and title
+   * @method isEmpty
+   * @param {Object} arrayObject
+   * @return {Boolean} Returns true if empty
+   * */
+  isEmpty(arrayObject) {
+    if (typeof arrayObject === 'object') {
+      // it's an object
+      if ((Object.keys(arrayObject).length > 0)) {
+        //it has some contents
+        for (let key in arrayObject) {
+          if (arrayObject[key].hasOwnProperty('title') &&
+              arrayObject[key].hasOwnProperty('text')) {
+            //the content is an array of object with property text and title
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
   resolveSearchTerms() {
     for (let arg of arguments) {
       if (arg instanceof Object && typeof arg !== 'string') {
@@ -130,3 +156,5 @@ class InvertedIndex {
     }
   }
 }
+
+module.exports = InvertedIndex;
